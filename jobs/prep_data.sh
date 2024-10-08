0cfa4b9b612c71893baa85fc674528e416586f95
matgrand <matteo.grandin@gmail.com>
2024-10-08 19:06:04 +0200

    +

--- jobs/prep_data.sh
+++ jobs/prep_data.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 #SBATCH --job-name=prep_data
-#SBATCH --error=output.%j.err
-#SBATCH --output=output.%j.out
+#SBATCH --error=jobs/output.%j.txt
+#SBATCH --output=jobs/output.%j.txt
 #SBATCH --partition=allgroups
 #SBATCH --ntasks=1
 #SBATCH --mem=20G
-#SBATCH --time=00:22:00
+#SBATCH --time=00:59:00
 cd $HOME/repos/PlaNet_Equil_reconstruction
-echo "Starting job"
 srun jupyter nbconvert prepare_dataset.ipynb --to python && python prepare_dataset.py && rm -rf prepare_dataset.py
-echo "Job finished"
