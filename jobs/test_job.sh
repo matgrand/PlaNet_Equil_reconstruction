87b4cc346d30fee1b5bb068d5be893a691314e5b
matgrand <matteo.grandin@gmail.com>
2024-10-09 11:34:51 +0200

    +

--- jobs/test_job.sh
+++ jobs/test_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=test
-#SBATCH --error=jobs/%j/output.txt
-#SBATCH --output=jobs/%j/output.txt
+#SBATCH --error=jobs/output%j.txt
+#SBATCH --output=jobs/output%j.txt
 #SBATCH --partition=allgroups
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
--- jobs/train.sh
+++ jobs/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=train
-#SBATCH --error=jobs/output.%j.txt
-#SBATCH --output=jobs/output.%j.txt
+#SBATCH --error=jobs/%j/output.txt
+#SBATCH --output=jobs/%j/output.txt
 #SBATCH --partition=allgroups
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
@@ -9,4 +9,6 @@
 #SBATCH --time=10:00:00
 #SBATCH --gres=gpu:a40:1
 cd $HOME/repos/PlaNet_Equil_reconstruction
+mkdir -p jobs/$SLURM_JOB_ID
+echo "running job $SLURM_JOB_ID"
 srun git pull && jupyter nbconvert mg_train.ipynb --to python && python mg_train.py && rm -rf mg_train.py
