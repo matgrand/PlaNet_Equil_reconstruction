e746ec95550b600a72851e0769b75bd64ea1a1c4
matgrand <matteo.grandin@gmail.com>
2024-10-09 11:36:43 +0200

    +

--- jobs/test_job.sh
+++ jobs/test_job.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=test
-#SBATCH --error=jobs/output%j.txt
-#SBATCH --output=jobs/output%j.txt
+#SBATCH --error=jobs/%j.txt
+#SBATCH --output=jobs/%j.txt
 #SBATCH --partition=allgroups
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 #SBATCH --time=00:01:00
 cd $HOME/repos/PlaNet_Equil_reconstruction
-mkdir -p jobs/$SLURM_JOB_ID
 echo "running job $SLURM_JOB_ID"
 srun python test_job_n.py
--- jobs/train.sh
+++ jobs/train.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=train
-#SBATCH --error=jobs/%j/output.txt
-#SBATCH --output=jobs/%j/output.txt
+#SBATCH --error=jobs/%j.txt
+#SBATCH --output=jobs/%j.txt
 #SBATCH --partition=allgroups
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
@@ -9,6 +9,5 @@
 #SBATCH --time=10:00:00
 #SBATCH --gres=gpu:a40:1
 cd $HOME/repos/PlaNet_Equil_reconstruction
-mkdir -p jobs/$SLURM_JOB_ID
 echo "running job $SLURM_JOB_ID"
 srun git pull && jupyter nbconvert mg_train.ipynb --to python && python mg_train.py && rm -rf mg_train.py
