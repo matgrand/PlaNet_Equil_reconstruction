f886c43bf2fb70bd3a5bf87c568ef1b0a838fee2
matgrand <matteo.grandin@gmail.com>
2024-10-09 11:19:19 +0200

    +

--- jobs/test_job.sh
+++ jobs/test_job.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=test
-#SBATCH --error=jobs/output.%j.txt
-#SBATCH --output=jobs/output.%j.txt
+#SBATCH --error=jobs/%j/output.txt
+#SBATCH --output=jobs/%j/output.txt
 #SBATCH --partition=allgroups
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=64G
 #SBATCH --time=00:01:00
 cd $HOME/repos/PlaNet_Equil_reconstruction
-mkdir -p jobs/%j
-echo "running job %j"
-srun python test_job_n.py -c=config/config_train_mg.yml -n=%j
+mkdir -p jobs/$SLURM_JOB_ID
+echo "running job $SLURM_JOB_ID"
+srun python test_job_n.py
