78a1e88950b9a08ba9a356596d637ef6e0654271
matgrand <matteo.grandin@gmail.com>
2024-10-14 16:32:50 +0200

    +

--- monitor_train_cluster.py
+++ monitor_train_cluster.py
@@ -5,5 +5,5 @@ for _ in range(23*60*2): #run for 23 hours
     files = os.listdir(dir) #list all the files in the directory
     files.sort(key=lambda x: os.path.getmtime(dir + "/" + x)) #sort the files by the time they were last modified
     with open(dir + "/" + files[-1]) as f: print(f.read()) #print everything in the most recently modified file
-    print(files[-1])
+    print(files[-1].split(".")[0]) #print the name of the most recently modified file
     sleep(30)
