ffabbac20824918d301503708e477aca7f9603dd
matgrand <matteo.grandin@gmail.com>
2024-10-14 11:39:43 +0200

    +

--- monitor_train_cluster.py
+++ monitor_train_cluster.py
@@ -1,18 +1,8 @@
 import os
 from time import sleep
-
-for _ in range(23*60*2):
+for _ in range(23*60*2): #run for 23 hours
     dir = "jobs"
-    #read all the files in the directory
-    files = os.listdir(dir)
-    #sort the files by the time they were created
-    files.sort(key=lambda x: os.path.getmtime(dir + "/" + x))
-    #get the last file
-    last_file = files[-1]
-    #print the last file
-    print(last_file)
-    #print everything in the last file
-    with open(dir + "/" + last_file) as f:
-        print(f.read())
-    #sleep for 30 seconds
+    files = os.listdir(dir) #list all the files in the directory
+    files.sort(key=lambda x: os.path.getmtime(dir + "/" + x)) #sort the files by the time they were last modified
+    with open(dir + "/" + files[-1]) as f: print(f.read()) #print everything in the most recently modified file
     sleep(30)
